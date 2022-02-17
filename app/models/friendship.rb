d194cb413cccb9dd92f236a91921b39150b6f731
bqgama <bqgama@gmail.com>
2022-02-17 14:57:38 -0300

    created validation to users not follow themselves

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,8 +1,17 @@
 class Friendship < ApplicationRecord
+  validate :not_follow_myself
+
   belongs_to :followers, class_name: 'User', foreign_key: :follower_id
   belongs_to :followings, class_name: 'User', foreign_key: :following_id
 
   scope :users_im_following, ->(user_id) {
     where(follower_id: user_id)
   }
+
+  private
+  def not_follow_myself
+    if follower_id == following_id
+      errors.add(:following_id, "You can't follow yourself")
+    end
+  end
 end
